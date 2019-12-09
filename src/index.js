45f52e0ee7ef913d19c5e92b838b7c614c7a9f4b
kiki <772765729@qq.com>
2019-12-09 20:49:18 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,40 +4,68 @@ import "./index.css";
 
 
 function App() {
-  const [breakLength, setBreakLength] = useState(parseInt(localStorage.getItem("BL")) || 1);
   const navList = ["welcome", "projects", "contact"]
-
-  useEffect(() => {
-    localStorage.setItem("BL", breakLength);
-  });
+  const projects = [
+    { name: 'calculator', displayName: '计算器', image: require("./img/calculator.png"), url: "http://chenyiran.top/react-calculator/build/index.html" },
+    { name: 'pomodoro', displayName: '番茄钟', image: require("./img/pomodoro.png"), url: "http://chenyiran.top/react-pomodoro/build/index.html" },
+    { name: 'tictactoe', displayName: '井字棋', image: require("./img/tictactoe.png"), url: "http://chenyiran.top/tic-tac-toe/build/index.html" },
+    { name: 'react', displayName: 'React文档示例', image: require("./img/react.png"), url: "http://chenyiran.top/React-Docs-examples/build/index.html" },
+    { name: 'vue', displayName: 'Vue文档示例', image: require("./img/vue.png"), url: "http://chenyiran.top/Vue-Docs-Examples/dist/index.html" },
+  ]
+  const contact = [
+    { name: 'github', displayName: "GitHub", url: "https://github.com/sumucheng" },
+    { name: 'email', displayName: "Email", content: '772765729@qq.com' },
+    { name: 'phone', displayName: "Call", content: '173-1782-4743' },
+  ]
 
   return (
     <div className="app">
-      <div id="navbar">
-        <nav>{navList.map(i => <li key={i}><a href={`#${i}`}>{i}</a></li>)}</nav>
-      </div>
-      <div id="welcome">
-        <h1>Hey I am Ryoko</h1>
-      </div>
-      <div id="projects">
-        <h2 className="projects-header">These are some of my projects</h2>
-        <div className="projects-grid">
-          <a href="" target="_black">
-            <img src={require("./img/calculator.png")} alt="project" className="project-image" />
-            <p><span className="code">&lt;</span>计算器<span className="code">&#47;&gt;</span></p>
-          </a>
+      <nav id="navbar">
+        <ul>{navList.map(i => <li key={i}><a href={`#${i}`}>{i}</a></li>)}</ul>
+      </nav>
+      <main id="main">
+        <div id="welcome">
+          <h1>Hey I am Ryoko</h1>
+          <p>Welcome to my channel :D</p>
         </div>
-        <div>
-          <a href="" target="_black">Show All</a>
+        <div id="projects">
+          <h2 className="projects-header">These are some of my projects</h2>
+          <div className="projects-grid">
+            {projects.map(project =>
+              <div className="project" key={project.name}>
+                <a href={project.url} target="_black" >
+                  <img src={project.image} alt="project" className="project-image" />
+                  <p>
+                    <span className="code">&lt;</span>
+                    {project.displayName}
+                    <span className="code">&#47;&gt;</span>
+                  </p>
+                </a>
+              </div>
+            )}
+          </div>
+          <div className="projects-show-all">
+            <a href="https://github.com/sumucheng" target="_black" className="show-all">Show All</a>
+          </div>
         </div>
-      </div>
 
-      <div id="contact">
-        <div>
-          <a href="https://github.com/sumucheng" target="_blank" >GitHub</a>
+        <div id="contact">
+          {contact.map(item =>
+            <div>
+              <div className='contact-info'>
+                <svg className="icon" aria-hidden="true">
+                  <use xlinkHref={`#icon-${item.name}`}></use>
+                </svg>
+                {item.url ?
+                  <a href={item.url} target="_blank" >{item.displayName}</a>
+                  : item.displayName}
+              </div>
+              {item.content && <div>{item.content}</div>}
+            </div>
+          )}
         </div>
-      </div>
-    </div>
+      </main>
+    </div >
   );
 }
 
